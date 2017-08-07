79b3b727d2648348e3ce89c9ec050d005e60b2f8
徐磊 <xulei@mpbxl.local>
2017-08-07 15:41:11 +0800

    user module
    用户模块

--- src/page/user-center/index.js
+++ src/page/user-center/index.js
@@ -1,8 +1,8 @@
 /*
- * @Author: xulei
- * @Date:   2017-08-07 01:43:17
+ * @Author: Rosen
+ * @Date:   2017-05-23 19:33:33
  * @Last Modified by:   xulei
- * @Last Modified time: 2017-08-07 02:31:23
+ * @Last Modified time: 2017-08-07 15:39:10
  */
 
 'use strict';
@@ -11,32 +11,33 @@ require('page/common/nav/index.js');
 require('page/common/header/index.js');
 var navSide = require('page/common/nav-side/index.js');
 var _share = require('util/share.js');
-var _user = require('service/user-service.js')
+var _user = require('service/user-service.js');
 var templateIndex = require('./index.string');
 
 // page 逻辑部分
 var page = {
-	init: function() {
-		this.onLoad();
-	},
-	onLoad: function() {
-		// 初始化左侧菜单
-		navSide.init({
-			name: 'user-center'
-		});
-		// 加载用户信息
-		this.loadInfo();
-	},
-
-	loadInfo: function() {
-		_user.getUserInfo(function(res) {
-			userHtml = _share.renderHtml(templateIndex, res);
-			$('.panel-body').html(userHtml);
-		}, function(errMsg) {
-			_share.errorTips(errMsg);
-		});
-	}
+    init: function() {
+        this.onLoad();
+    },
+    onLoad: function() {
+        // 初始化左侧菜单
+        navSide.init({
+            name: 'user-center'
+        });
+        // 加载用户信息
+        this.loadUserInfo();
+    },
+    // 加载用户信息
+    loadUserInfo: function() {
+        var userHtml = '';
+        _user.getUserInfo(function(res) {
+            userHtml = _share.renderHtml(templateIndex, res);
+            $('.panel-body').html(userHtml);
+        }, function(errMsg) {
+            _share.errorTips(errMsg);
+        });
+    }
 };
 $(function() {
-	page.init();
+    page.init();
 });
